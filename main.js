1e500a62b85139fb9c220c8ee74f1ffa1e5dda04
Paul <pauljonaskamp@gmx.de>
2023-01-19 21:09:05 +0100

    ganz viele tolle Änderungen

--- main.js
+++ main.js
@@ -8,19 +8,19 @@ window.addEventListener('scroll', () =>{
     if (offset > 700) {
         nav.style.height = '6rem';
         navbk.style.backgroundColor = 'white';
-
         for (let i = 0; i < navItem.length; i++) {
             navItem[i].style.color = 'var(--darkBlue)';
             navItem[i].style.fontFamily  = 'var(--normal)';
         }
+        
     }else{
         nav.style.height = '5rem';
         navbk.style.backgroundColor = 'transparent';
-
         for (let i = 0; i < navItem.length; i++) {
             navItem[i].style.color = 'white';
             navItem[i].style.fontFamily  = 'var(--extraLight)';
         }
+        
     }
 
 })
